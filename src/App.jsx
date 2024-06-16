75653cc123efc6929c3f0184cc43df9ebb4fffd6
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2024-06-16 12:23:01 +0530

    css and functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumber] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(true)
+  const [password, setPassword] = useState("")
+  
+  const passwordRef=useRef(null) 
+
+  const copyToClipboard=useCallback(()=>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,99)
+
+    window.navigator.clipboard.writeText(password);
+  },[password])
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&+=_-{}[]~`"
+    for (let i = 1; i <=length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+
+  }, [numberAllowed, charAllowed, setPassword, length])
+  useEffect(()=>{
+    passwordGenerator()
+  },[numberAllowed, charAllowed, passwordGenerator, length ])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-full max-w-md mx-auto shawdow-md  py-4 px-4 my-8 text-orange-500 bg-gray-700  rounded-lg'>
+        <h1 className='text-white text-center  py-2'>Password Generator</h1>
+        <div className='flex items-center space-x-2'>
+          <input
+            type="text"
+            value={password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='Password'
+            readOnly
+            ref={passwordRef}
+          />
+          <button 
+             onClick={copyToClipboard}
+            className='outline-none bg-blue-800 text-white px-3 py-0.5 shrink-0'
+           
+          >
+            Copy
+          </button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input type="range" min={6} 
+            max={100}
+            value={length}
+            className='cursor-pointer'
+            onChange={(e)=>{setLength(e.target.value)}}
+            />
+            {/*here e stands for event and we are passing it in setLength defined in use stateof length  */}
+           
+            <label> Length: {length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={()=>{
+              setNumber((prev)=>!prev);
+
+            }} />
+            <label htmlFor="numberInput">Numbers</label>
+            <input type="checkbox" id="characterInput"
+            defaultChecked={charAllowed}
+            onChange={()=>{
+              setCharAllowed((prev)=>!prev )
+            }} />
+            <label htmlFor="characterInput">Character</label>
+          </div>
+        </div>
+       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
